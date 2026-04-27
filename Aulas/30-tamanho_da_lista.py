014c41aa7e44209f6d967fe01f7a6655941b54b0
Thiago Copetti <202411043535@alunos.estacio.br>
2026-04-27 14:34:00 -0300

    aulas 29 e 30

---
+++ Aulas/30-tamanho_da_lista.py
@@ -0,0 +1,19 @@
+# Tamanho de uma Lista em Python
+
+# Criando uma lista de números
+numeros = [1, 2, 3, 4, 5]
+
+# Descobrindo o tamanho da lista usando a função len()
+# tamanho = len(lista)
+
+tamanho_numeros = len(numeros)
+print(f"O tamanho da lista de números é: {tamanho_numeros}")  # Output
+
+# maior valor da lista
+maior_numero = max(numeros)
+print(f"O maior número da lista é: {maior_numero}")  # Output: O maior número da lista é: 5
+
+# menor valor da lista
+menor_numero = min(numeros)
+print(f"O menor número da lista é: {menor_numero}")  # Output: O menor número da lista é: 1
+
